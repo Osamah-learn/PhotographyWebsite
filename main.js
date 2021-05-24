d3e802ef37bf7dc19f699a0678120fbbe97775e0
Osamah-learn <77615959+Osamah-learn@users.noreply.github.com>
2021-05-24 21:26:41 +0300

    WebSite is done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("Javascript");
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
-const links = nav.querySelector("a");
+const links = nav.querySelectorAll("a");
 
 burger.addEventListener("click", ()=>{
 nav.classList.toggle("nav-open")
